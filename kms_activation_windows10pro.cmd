d610999dc78a20c93bbdf8249a8cb99fa2c7af54
Sakibur Rahman <sakibmas@gmail.com>
2021-01-05 07:22:32 +0600

    Create kms_activation_windows10pro.cmd
    window 10 professional

---
+++ kms_activation_windows10pro.cmd
@@ -0,0 +1,4 @@
+@echo off
+cscript c:\windows\system32\slmgr.vbs -skms kms.sakib.biz
+cscript c:\windows\system32\slmgr.vbs -ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+cscript c:\windows\system32\slmgr.vbs -ato
