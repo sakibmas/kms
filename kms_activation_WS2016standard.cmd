a9bdb0d5501c8bf64ff293a32446d38a536dd964
Sakibur Rahman <sakibmas@gmail.com>
2021-04-26 01:38:09 +0600

    Add files via upload

---
+++ kms_activation_WS2016standard.cmd
@@ -0,0 +1,4 @@
+@echo off
+cscript c:\windows\system32\slmgr.vbs -skms kms.sakib.biz
+cscript c:\windows\system32\slmgr.vbs -ipk WC2BQ-8NRM3-FDDYY-2BFGV-KHKQY
+cscript c:\windows\system32\slmgr.vbs -ato
